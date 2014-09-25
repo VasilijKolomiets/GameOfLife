62b5b20d1aaf055ba176f50cc911675a5465adf3
Max Zuev <greeneyedtramp@yandex.ru>
2014-09-25 21:38:48 +0400

    Add is_alive_con function and tests for it

--- main.py
+++ main.py
@@ -23,14 +23,19 @@ def calculate_alive_neighbors(con, alive_cons):
                       get_neighbors(con)))
 
 
+def is_alive_con(con, alive_cons):
+    alive_neighbors = calculate_alive_neighbors(con, alive_cons)
+    if (alive_neighbors == 3 or
+            (alive_neighbors == 2 and con in alive_cons)):
+        return True
+    return False
+
+
 def new_step(alive_cons):
     board = itertools.chain(*map(get_neighbors, alive_cons))
-    new_board = set()
-    for con in board:
-        alive_neighbors = calculate_alive_neighbors(con, alive_cons)
-        if (alive_neighbors == 3 or
-                (alive_neighbors == 2 and con in alive_cons)):
-            new_board.add(con)
+    new_board = set([con
+                     for con in board
+                     if is_alive_con(con, alive_cons)])
     return list(new_board)
 
 
--- test_main.py
+++ test_main.py
@@ -65,6 +65,50 @@ def test_calculate_alive_neighbors():
     assert main.calculate_alive_neighbors(con, alive_cons) == 3
 
 
+class TestIsAliveCon(object):
+    def test_new_con(self):
+        alive_cons = [(1, 1),
+                      (2, 0),
+                      (2, 2)]
+        con = (2, 1)
+        assert main.is_alive_con(con, alive_cons) is True
+
+    def test_alive_con_alive_3_neighbors(self):
+        alive_cons = [(1, 1),
+                      (2, 0),
+                      (2, 1),
+                      (2, 2)]
+        con = (2, 1)
+        assert main.is_alive_con(con, alive_cons) is True
+
+    def test_alive_con_alive_2_neighbors(self):
+        alive_cons = [(1, 1),
+                      (2, 0),
+                      (2, 1)]
+        con = (2, 1)
+        assert main.is_alive_con(con, alive_cons) is True
+
+    def tests_dead_con_few_neighbor(self):
+        alive_cons = [(1, 1),
+                      (2, 0)]
+        con = (2, 1)
+        assert main.is_alive_con(con, alive_cons) is False
+
+    def test_alive_con_few_neighbors(self):
+        alive_cons = [(1, 1),
+                      (2, 1)]
+        con = (2, 1)
+        assert main.is_alive_con(con, alive_cons) is False
+
+    def test_many_neighbors(self):
+        alive_cons = [(1, 0),
+                      (1, 1),
+                      (2, 0),
+                      (3, 1)]
+        con = (2, 1)
+        assert main.is_alive_con(con, alive_cons) is False
+
+
 class TestIsCorrectCon(object):
     def test_zero_zero(self):
         assert main.is_correct_con(5, (0, 0)) is True
